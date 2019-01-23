f17073bfe7536b2314c8dc6f1228c4a23f2bc185
juansmolano <juansmolano@gmail.com>
2019-01-23 17:35:39 -0500

    first commit

---
+++ deployment/docker/build-docker-hub.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+export DOCKERHUB_IMAGE=drivergateway
+export DOCKERHUB_TAG=0.0.15
+
+rm -rf deployment/docker/driver-gateway/
+cp -R $API_SHELL_PATH/driver-gateway deployment/docker/driver-gateway
+
+docker build -m "300M" -t $DOCKERHUB_NAMESPACE/$DOCKERHUB_IMAGE:$DOCKERHUB_TAG -t $DOCKERHUB_NAMESPACE/$DOCKERHUB_IMAGE:latest deployment/docker/
+docker login -u $DOCKERHUB_USER -p $DOCKERHUB_PASS
+docker push $DOCKERHUB_NAMESPACE/$DOCKERHUB_IMAGE:$DOCKERHUB_TAG
+docker push $DOCKERHUB_NAMESPACE/$DOCKERHUB_IMAGE:latest
