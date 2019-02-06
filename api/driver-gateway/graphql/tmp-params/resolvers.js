5eae01d829e41065154258acc957d7f137a9d818
juansmolano <juansmolano@gmail.com>
2019-02-06 10:48:13 -0500

    Configure params from kubernetes secrets

--- api/driver-gateway/graphql/tmp-params/resolvers.js
+++ api/driver-gateway/graphql/tmp-params/resolvers.js
@@ -8,11 +8,11 @@ module.exports = {
     MqttParams: (root, args, context, info) => {
       return of(
         {
-          url: "ws://35.222.170.20",
-          port: 8083,
+          url: process.env.DRIVER_APP_MQTT_DRIVER_URL,
+          port: parseInt(process.env.DRIVER_APP_MQTT_DRIVER_PORT),
           clientId: context.authToken.preferred_username,
-          user: "",
-          password: "",
+          user: process.env.DRIVER_APP_MQTT_DRIVER_USER,
+          password: process.env.DRIVER_APP_MQTT_DRIVER_PASSWORD,
         }
       )
         .toPromise()
