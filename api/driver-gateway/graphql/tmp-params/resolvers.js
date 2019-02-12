f451f1697563e5b8082a539031a884e12f5c36f3
juansmolano <juansmolano@gmail.com>
2019-02-12 15:36:48 -0500

    Grouped Google & Mqtt params in a single Query: Params

--- api/driver-gateway/graphql/tmp-params/resolvers.js
+++ api/driver-gateway/graphql/tmp-params/resolvers.js
@@ -4,28 +4,39 @@ const request = require('request');
 
 
 
+const buildMqttParams = (context) => {
+  const mqttParamsCount = process.env.DRIVER_APP_MQTT_DRIVER_URL.split(';').length;
+  let i;
+  let params = [];
+  for (i = 0; i < mqttParamsCount; i++) {
+    params.push(
+      {
+        url: process.env.DRIVER_APP_MQTT_DRIVER_URL.split(';')[i],
+        port: parseInt(process.env.DRIVER_APP_MQTT_DRIVER_PORT.split(';')[i]),
+        clientId: context.authToken.preferred_username,
+        user: process.env.DRIVER_APP_MQTT_DRIVER_USER.split(';')[i],
+        password: process.env.DRIVER_APP_MQTT_DRIVER_PASSWORD.split(';')[i],
+        order: i
+      },
+    );
+  };
+  return params;
+};
+
+const buildGoogleMapsParams = () => {
+  return {
+    googleMapsAndroidKey: process.env.GOOGLE_MAPS_ANDROID_KEY,
+    googleMapsBrowserKey: process.env.GOOGLE_MAPS_BROWSER_KEY
+  };
+}
+
 
 module.exports = {
 
   Query: {
 
     MqttParams: (root, args, context, info) => {
-      const mqttParamsCount = process.env.DRIVER_APP_MQTT_DRIVER_URL.split(';').length;
-      let i;
-      let params = [];
-      for (i = 0; i < mqttParamsCount; i++) {
-        params.push(
-          {
-            url: process.env.DRIVER_APP_MQTT_DRIVER_URL.split(';')[i],
-            port: parseInt(process.env.DRIVER_APP_MQTT_DRIVER_PORT.split(';')[i]),
-            clientId: context.authToken.preferred_username,
-            user: process.env.DRIVER_APP_MQTT_DRIVER_USER.split(';')[i],
-            password: process.env.DRIVER_APP_MQTT_DRIVER_PASSWORD.split(';')[i],
-            order: i
-          },
-        );
-      }
-      return of(params).toPromise()
+      return of(buildMqttParams(context)).toPromise()
     },
 
 
@@ -35,6 +46,15 @@ module.exports = {
         googleMapsBrowserKey: process.env.GOOGLE_MAPS_BROWSER_KEY
       }).toPromise()
     },
+
+
+    Params: (root, args, context, info) => {
+      return of({
+        GoogleMapsParams: buildGoogleMapsParams(),
+        MqttParams: buildMqttParams(context)
+      }).toPromise()
+    },
+
   },
 }
 
